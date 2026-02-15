8a25e561b43c24a5c988837e768644a0a2e8ddb2
Jihoon Oh <oh@jsk.imi.i.u-tokyo.ac.jp>
2026-02-15 22:59:35 +0900

    modify training scripts

--- training_scripts/aff_r1.sh
+++ training_scripts/aff_r1.sh
@@ -12,11 +12,11 @@ python3 -m verl.trainer.main \
     worker.actor.model.model_path=${MODEL_PATH} \
     worker.actor.kl_loss_coef=5.0e-3 \
     worker.actor.optim.lr=1.0e-6 \
-    worker.actor.micro_batch_size_per_device_for_update=8\
-    worker.actor.micro_batch_size_per_device_for_experience=16\
+    worker.actor.micro_batch_size_per_device_for_update=2\
+    worker.actor.micro_batch_size_per_device_for_experience=2\
     worker.rollout.enable_chunked_prefill=false \
     worker.rollout.n=8 \
     trainer.experiment_name=affordacneR1 \
-    trainer.n_gpus_per_node=4 \
+    trainer.n_gpus_per_node=2 \
     trainer.total_episodes=5 \
-    trainer.save_checkpoint_path=/model/aff_r1
+    trainer.save_checkpoint_path=./outputs/aff_r1
